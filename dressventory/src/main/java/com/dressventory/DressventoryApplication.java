5ab54ed7f9a4493037337b982b192bab1ada6e6b
priyasuman <priyasuman776@gmail.com>
2025-07-07 21:45:47 +0530

    this is the initial project generated by spring initializer with jdk21, springg  3.5.3 dependencies -> jpa, spring web, dev tools, lambok

---
+++ dressventory/src/main/java/com/dressventory/DressventoryApplication.java
@@ -0,0 +1,13 @@
+package com.dressventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DressventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DressventoryApplication.class, args);
+	}
+
+}
---
+++ dressventory/src/test/java/com/dressventory/DressventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dressventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DressventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
